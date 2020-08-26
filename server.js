d204bf070ff0d8e20e97432b7f9efac64cef4e7c
Gabriel Ruiseco <gabriel_alejandro_rc@outlook.com>
2020-08-26 05:18:35 -0500

    "update"

--- server.js
+++ server.js
@@ -21,5 +21,16 @@ const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
+  .wsServer()
   .fireHttpServer()
   .catch(console.error)
+
+const cluster = require('cluster')
+
+if (cluster.isMaster) {
+  for (let i=0; i < 4; i ++) {
+    cluster.fork()
+  }
+  require('@adonisjs/websocket/clusterPubSub')()
+  return
+}
